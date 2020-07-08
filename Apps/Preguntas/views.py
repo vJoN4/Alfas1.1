4eeedffda73a19f195c3c8e55df94be6d4b1e4c8
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2020-07-07 22:52:43 -0500

    *feat* show questions functions to display questions and answers, and to upload questions

--- Apps/Preguntas/views.py
+++ Apps/Preguntas/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import pregunta, respuesta
+from datetime import date
+
 
 # Create your views here.
+def renderHome(request):
+    return render(request, 'homeBase.html')
+
+def renderQuestions(request):
+    allQuestions = pregunta.objects.all().order_by('-fecha')
+    return render(request, 'questions.html', {'questions': allQuestions})
+
+def renderAnswers(request, idPregunta):
+    question = pregunta.objects.get(id=idPregunta)
+    answers = respuesta.objects.filter(pregunta__id=idPregunta)
+    return render(request, 'answers.html', {
+        'question':question,
+        'answers' : answers,
+    })
+
+def uploadQuestionTemplate(request):
+    allQuestions = pregunta.objects.all().order_by('-fecha')
+    # print(request.user.id)
+    return render(request, 'uploadQuestion.html', {'questions': allQuestions})
+
+def uploadQuestion(request):
+    try:
+        contenido = request.POST['contenido']
+        descripcion = request.POST['descripcion']
+        categoria = request.POST['categoria']
+
+        print(contenido)
+        print(descripcion)
+        print(categoria)
+        print(request.user.id)
+        print(date.today())
+        pregunta.objects.create(
+            contenido = contenido,
+            descripcion = descripcion,
+            fecha = date.today(),
+            categoria = categoria,
+            usuario = request.user,
+        )
+        return HttpResponse(True)
+    except Exception as identifier:
+        return HttpResponse(False)
