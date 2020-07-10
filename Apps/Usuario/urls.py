d83dbdf095734d22cb80a7ea79e86e990a9d60b5
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2020-07-10 12:12:41 -0500

    *feat* paths: home and logout

--- Apps/Usuario/urls.py
+++ Apps/Usuario/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views as views_usuario
 
 urlpatterns = [
-    path('', views_usuario.Login, name='login'),
+    path('', views_usuario.home, name='home'),
+    path('login', views_usuario.Login, name='login'),
     path('registro', views_usuario.register, name='registro'),
+    path('logout', views_usuario.Logout, name='logout'),
 ]
