9751d469041e160243c8b868c87dd861dc83383a
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2020-07-10 12:09:04 -0500

    *feat* paths: profile, user-questions, edit/delete question, answer, editprofile and category filter

--- Apps/Preguntas/urls.py
+++ Apps/Preguntas/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('addquestionurl', views_questions.uploadQuestion, name='upQuestionURL'),
     path('addanswers/<int:idPregunta>', views_questions.uploadAnswersTemplate, name='upAnswer'),
     path('addanswersurl', views_questions.uploadAnswers, name='upAnswerURL'),
+    path('profile', views_questions.renderProfile, name='profile'),
+    path('myquestions', views_questions.renderUserQuestions, name='myquestions'),
+    path('editquestionsurl', views_questions.editQuestions, name='editquestionsurl'),
+    path('deletequestion', views_questions.deleteQuestion, name='deletequestionurl'),
+    path('editanswer',views_questions.editAnswer, name='editanswerurl'),
+    path('deleteanswer',views_questions.deleteAnswer, name='deleteanswerurl'),
+    path('editprofile', views_questions.editProfile, name='editprofileurl'),
+    path('bycategory/<str:categoria>', views_questions.renderQuestionsByCategory, name='bycategory'),
 ]
