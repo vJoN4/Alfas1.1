3fe4268b99f42233189207854da0b59e56070267
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2020-07-10 14:40:54 -0500

    *feat*imagefield->null, blank ->True

--- Apps/Usuario/models.py
+++ Apps/Usuario/models.py
@@ -6,4 +6,4 @@ def user_directory_path(instance, filename):
     return '/'.join(['profile_pictures/', instance.username+'.jpg'])
 
 class User(AbstractUser):
-    foto = models.ImageField(upload_to=user_directory_path, default='default.jpg')
+    foto = models.ImageField(upload_to=user_directory_path, default='default.jpg', blank=True, null=True)
